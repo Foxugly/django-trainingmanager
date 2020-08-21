ed6f8490ca8db4518c820f648b6b1543b7e165f2
foxugly <rvilain@skynet.be>
2020-08-21 17:47:21 +0200

    add fields to member

--- member/models.py
+++ member/models.py
@@ -1,15 +1,18 @@
-from tools.generic_class import GenericClass
 from django.db import models
 from django.utils.translation import gettext as _
+
+from tools.generic_class import GenericClass
 
 
 # Create your models here.
 class Member(GenericClass):
     firstname = models.CharField(max_length=100, verbose_name=_("Firstname"), )
     lastname = models.CharField(max_length=100, verbose_name=_("Lastname"), )
+    phonenumber = models.CharField(max_length=20, blank=True, null=True, verbose_name=_("Phonenumber"), )
+    email = models.EmailField(max_length=50, blank=True, null=True, verbose_name=_("Email"), )
 
     def get_fullname(self):
-    	return "%s %s" % (self.firstname, self.lastname)
+        return "%s %s" % (self.firstname, self.lastname)
 
     def __str__(self):
         return self.get_fullname()
