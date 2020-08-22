e13e6717ae417584475d94f85bfc1ee258a15ac5
Foxugly <rvilain@skynet.be>
2020-08-23 01:06:38 +0200

    Update urls.py

--- event/urls.py
+++ event/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from event.views import PDFEventView, EventRawView, member_attendance
+from event.views import PDFEventView, EventRawView, attendance_member
 from tools.generic_urls import add_url_from_generic_views
 
 app_name = 'event'
 urlpatterns = add_url_from_generic_views('event.views')
 urlpatterns.append(path('event/<int:pk>/raw/', EventRawView.as_view(), name="event_raw"))
 urlpatterns.append(path('event/<int:pk>/pdf/', PDFEventView.as_view(), name="event_pdf"))
-urlpatterns.append(path('event/<int:pk>/member/<int:member_id>/attendance/', member_attendance, name="member_attendance"))
+urlpatterns.append(path('event/<int:event_id>/member/<int:member_id>/attendance/', attendance_member, name="attendance_member"))
